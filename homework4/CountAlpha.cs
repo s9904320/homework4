2b4f3adb74e3193b0a53748d1eae8309beaf9912
s9904320 <91059581+s9904320@users.noreply.github.com>
2022-01-30 15:04:47 +0800

    Add project files.

---
+++ homework4/CountAlpha.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace homework4
+{
+    public class CountAlpha
+    {
+        public static int CountAlphaFunction(string insertStr)
+        {
+            int intDuplicate = 0;
+            Dictionary<string, int> countDic = new Dictionary<string, int>();
+            if (string.IsNullOrEmpty(insertStr))
+            {
+                return intDuplicate;
+            }
+            else
+            {
+                for(int i = 0; i < insertStr.Length; i++)
+                {
+                    if (countDic.ContainsKey(insertStr.Substring(i, 1).ToLower()))
+                    {
+                        countDic[insertStr.Substring(i, 1).ToLower()] ++;
+                    }
+                    else
+                    {
+                        countDic.Add(insertStr.Substring(i, 1).ToLower(), 0);
+                    }               
+                }
+                foreach(var numCount in countDic)
+                {
+                    if(numCount.Value != 0)
+                    {
+                        intDuplicate ++;
+                    }
+                }
+            }
+            return intDuplicate;
+        }
+    }
+}
---
+++ homework4Tests/CountAlphaTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using homework4;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework4.Tests
+{
+    [TestClass()]
+    public class CountAlphaTests
+    {
+        [TestMethod()]
+        public void CountAlphaForNullOrEmpty()
+        {
+            Assert.AreEqual(0, CountAlpha.CountAlphaFunction(""));
+            Assert.AreEqual(0, CountAlpha.CountAlphaFunction(null));
+        }
+
+        [TestMethod()]
+        public void CountAlphaForNormal()
+        {
+            Assert.AreEqual(0, CountAlpha.CountAlphaFunction("abcde"));
+            Assert.AreEqual(2, CountAlpha.CountAlphaFunction("aabbcde"));
+        }
+
+        [TestMethod()]
+        public void CountAlphaForCase()
+        {
+            Assert.AreEqual(2, CountAlpha.CountAlphaFunction("aabBcde"));
+            Assert.AreEqual(1, CountAlpha.CountAlphaFunction("Indivisibility"));
+            Assert.AreEqual(2, CountAlpha.CountAlphaFunction("Indivisibilities"));
+        }
+
+    }
+}
